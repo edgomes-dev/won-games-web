7414b85b9ebbc74c08d2b36e50d6f1797196402f
edgomes-dev <edgomesgomes@outlook.com>
2023-05-02 12:32:58 -0300

    MediaMatch created

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MediaMatch } from '.';
+
+const meta: Meta<typeof MediaMatch> = {
+  title: 'MediaMatch',
+  component: MediaMatch
+};
+
+export default meta;
+type Story = StoryObj<typeof MediaMatch>;
+
+export const Desktop: Story = {
+  render: () => <MediaMatch greaterThan="medium">Only on Desktop</MediaMatch>
+};
+
+export const Mobile: Story = {
+  render: () => <MediaMatch lessThan="medium">Only on mobile</MediaMatch>
+};
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+};
