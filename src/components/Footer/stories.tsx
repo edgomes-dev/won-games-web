f4fc0dfe60890ac582eb5961b414c6b4008b701c
edgomes-dev <edgomesgomes@outlook.com>
2023-05-03 12:03:06 -0300

    Footer created

---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Footer } from '.';
+
+const meta: Meta<typeof Footer> = {
+  title: 'Footer',
+  component: Footer
+};
+
+export default meta;
+type Story = StoryObj<typeof Footer>;
+
+export const Primary: Story = {
+  render: () => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <Footer />
+    </div>
+  )
+};
